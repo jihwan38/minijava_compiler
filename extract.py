b99789c5c8c05c4015ccaaf17d7bee85a45f9ee6
jihwan38 <sss203800@gmail.com>
2026-04-04 20:40:01 +0900

    chore: 필요없는 파일들 정리

--- extract.py
+++
@@ -1,6 +0,0 @@
-import sys
-import PyPDF2
-reader = PyPDF2.PdfReader('3장 구문 분석.pdf')
-text = '\n'.join(page.extract_text() for page in reader.pages)
-with open('pdf_content.txt', 'w', encoding='utf-8') as f:
-    f.write(text)
