47b3cf38d949a362205c70a2a55746036060d300
jihwan38 <sss203800@gmail.com>
2026-06-29 14:54:17 +0900

    chore: Remove obsolete backup directories (chap11_1, chap11_init)

--- chap11_1/Mips/InFrame.java
+++
@@ -1,9 +0,0 @@
-package Mips;
-
-public class InFrame implements Access {
-  public final int offset;
-
-  public InFrame(int offset) {
-    this.offset = offset;
-  }
-}
--- chap11_init/Mips/Access.java
+++
@@ -1,3 +0,0 @@
-package Mips;
-
-public interface Access {}
